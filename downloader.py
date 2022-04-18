7b74e90f25b6636437bf463c6003a71a6d0ac5a5
ivanh <ivan.honis@gmail.com>
2022-04-18 07:56:14 +0200

    fix multiprocess bug, shared varialbe

--- downloader.py
+++ downloader.py
@@ -4,25 +4,21 @@ import psutil
 import pickle
 import time
 import os
-import multiprocessing, ctypes
+import multiprocessing
+from multiprocessing import Value, Manager
 from google.cloud import storage
 import gc
 import datetime
 import json
 
-
-
 # Binanace
 from binance import AsyncClient, BinanceSocketManager
-
-run_stream = multiprocessing.Value(ctypes.c_int, 0)  # (type, init value)
-
 
 class time_gates():
 
     def __init__(self):
         self.time_dict = {}
-        self.time_steps = 1
+        self.time_steps = 60 * 1.5
         self.get_time_dict()
         self.overlap = 10  # sec
 
@@ -30,24 +26,24 @@ class time_gates():
         a = datetime.datetime.now()
         x = int(a.minute)
         z = 1
-        while (x + z) % 2 != 0:
-            z += 1
+        # while (x + z) % 2 != 0:
+        #     z += 1
 
         c = a + datetime.timedelta(minutes=z)
         c = datetime.datetime(c.year, c.month, c.day, c.hour, c.minute)
-        print(a, c)
+        # print(a, c)
 
         time_dict = {}
 
         time_dict[0] = c
-        time_dict[1] = time_dict[0] + datetime.timedelta(minutes=self.time_steps)
-        time_dict[2] = time_dict[1] + datetime.timedelta(minutes=self.time_steps)
+        time_dict[1] = time_dict[0] + datetime.timedelta(seconds=self.time_steps)
+        time_dict[2] = time_dict[1] + datetime.timedelta(seconds=self.time_steps)
         self.time_dict = time_dict
 
     def shift_time_dict(self):
         self.time_dict[0] = self.time_dict[2]
-        self.time_dict[1] = self.time_dict[0] + datetime.timedelta(minutes=self.time_steps)
-        self.time_dict[2] = self.time_dict[1] + datetime.timedelta(minutes=self.time_steps)
+        self.time_dict[1] = self.time_dict[0] + datetime.timedelta(seconds=self.time_steps)
+        self.time_dict[2] = self.time_dict[1] + datetime.timedelta(seconds=self.time_steps)
 
     def get_time_flag(self, pos):
         if pos == 0:
@@ -65,7 +61,6 @@ class time_gates():
             elif self.time_dict[2] + datetime.timedelta(seconds=self.overlap) < datetime.datetime.now():
                 return 2
 
-
 class n_book_saver:
 
     def __init__(self):
@@ -74,10 +69,10 @@ class n_book_saver:
         self.b_client = None
         self.account = self.get_account()
         self.exchange_info = self.get_exchange_info()
-        self.time_pos = 0
+        self.time_pos = 0  # vagy 1 ha a másik időkapun megy át
         self.tg = time_gates()
         self.time_flag = 0
-        self.chk_delay = 4  # sec
+        self.chk_delay = 5  # sec
         self.stream_dict = {}
 
         # self.stram_chanel = True
@@ -87,6 +82,7 @@ class n_book_saver:
         # self.all_pos = 0
         self.last_save_name = ""
         self.proc = None
+        self.run_stream = 1
 
 
         # self.symbols = ['AGLD', 'STPT', 'MXN', 'UGX', 'RENBTC', 'GLM', 'RAY', 'NEAR', 'AUDIO', 'HNT', 'ADADOWN', 'CDT', 'SPARTA', 'SUSD', 'FARM', 'XNO', 'AION', 'NPXS', 'DGB', 'ZRX', 'BCD', 'EASY', 'SANTOS', 'WING', 'WNXM', 'BCH', 'JST', 'ADAUP', 'HOT', 'AR', 'IRIS', 'RAMP', 'BCX', 'SEK', 'TRIG', 'RCN', 'COVER', 'FLM', 'GNO', 'VITE', 'GNT', 'BKRW', 'CFX', 'XPR', 'SFP', 'DIA', 'RDN', 'ACA', 'ARDR', 'LOOMOLD', 'NEBL', 'ACH', 'SLPOLD', 'BEL', 'JUV', 'ACM', 'MINA', 'GRTDOWN', 'VTHO', 'PYROLD', 'SGB', 'SALT', 'STORM', 'REN', 'REP', 'ADA', 'ELF', 'REQ', 'STORJ', 'CHF', 'ADD', 'BZRX', 'SGT', 'DF', 'RARE', 'EOSDOWN', 'PAXG', 'YOYO', 'PAX', 'CHR', 'VND', 'BCHDOWN', 'WAVES', 'CHZ', 'ADX', 'XRP', 'WPR', 'JASMY', 'AED', 'FIDA', 'SAND', 'DKK', 'OCEAN', 'FOR', 'UMA', 'DREPOLD', 'SCRT', 'TUSD', 'EZ', 'TKO', 'WABI', 'RGT', 'IDRT', 'ENG', 'ENJ', 'UNIDOWN', 'YFII', 'KZT', 'OAX', 'GRT', 'GRS', 'UND', 'HARD', 'TFUEL', 'ENS', 'LEND', 'DLT', 'TROY', 'XLMUP', 'UNI', 'BTCDOWN', 'TLM', 'HUF', 'SBTC', 'CKB', 'WRX', 'XTZ', 'LUNA', 'ETHDOWN', 'AGI', 'BCHA', 'EON', 'EOP', 'EOS', 'GO', 'NCASH', 'RIF', 'NSBT', 'SKL', 'XDATA', 'GTC', 'PEN', 'BLINK', 'SOLO', 'SXPDOWN', 'HC', 'SKY', 'BURGER', 'NAS', 'NAV', 'GTO', 'WTC', 'XVG', 'EPS', 'DNT', 'CLV', 'FLOW', 'XTZDOWN', 'XVS', 'STEEM', 'BVND', 'SLP', 'VRT', 'NBS', 'DON', 'LAZIO', 'DOT', 'IQ', 'GRTUP', '1INCH', 'KNCL', 'CHESS', 'MITH', 'ERD', 'DEGO', 'CND', 'GYEN', 'UNFI', 'FTM', 'POWR', 'ERN', 'GVT', 'WINGS', 'FTT', 'VOXEL', 'PHA', 'RLC', 'PHB', 'TRXDOWN', 'ATOM', 'XRPUP', 'QUICK', 'BLZ', 'SNM', 'BOBA', 'MBL', 'MTLX', 'SNT', 'PHP', 'SNX', 'LTCDOWN', 'FUN', 'SNMOLD', 'COP', 'COS', 'API3', 'USD', 'QKC', 'SUSHIUP', 'ROSE', 'GLMR', 'XYM', 'PURSE', 'SOL', 'TRXUP', 'CITY', 'ETC', 'BNC', 'CELR', 'UST', 'OGN', 'ETH', 'NEO', 'TOMO', 'CELO', 'KLAY', 'AUCTION', 'BADGER', 'HIGH', 'GXS', 'TRB', 'BNT', 'QLC', 'LBA', 'MDA', 'BNX', 'UTK', 'WSOL', 'HEGIC', 'MA', 'AMB', 'MC', 'TRU', 'FUEL', 'DREP', 'TRY', 'TRX', 'MDT', 'NFT', 'MDX', 'XRPDOWN', 'AERGO', 'EUR', 'AMP', 'BOT', 'NULS', 'AUTO', 'NGN', 'ANC', 'BDOT', 'EGLD', 'ANTOLD', 'SPELL', 'PUNDIX', 'FXS', 'PLA', 'HNST', 'EVX', 'CRV', 'BAKE', 'ANT', 'NU', 'FLUX', 'ANY', 'LINKUP', 'SRM', 'QISWAP', 'TORN', 'PLN', 'QNT', 'ALICE', 'OG', 'MFT', 'OM', 'BTTOLD', 'BETH', 'BQX', 'WETH', 'PHBV1', 'BETA', 'BRD', 'SSV', 'BUSD', 'CTK', 'ARPA', 'DOTDOWN', 'BRL', 'ALCX', 'CTR', 'MATIC', 'IOTX', 'SHIB', 'TVK', 'FRONT', 'ZAR', 'DOCK', 'STX', 'PNT', 'QI', 'DENT', 'MBOX', 'SUB', 'POA', 'IOST', 'CAKE', 'ETHUP', 'POE', 'OMG', 'BAND', 'SUN', 'ASTR', 'SUNOLD', 'BTC', 'TWT', 'NKN', 'RSR', 'IOTA', 'CVC', 'REEF', 'BTG', 'MIR', 'KES', 'ARK', 'LOKA', 'CVP', 'ARN', 'KEY', 'BTS', 'SPARTAOLD', 'ARS', 'CVX', 'ONE', 'LINKDOWN', 'ONG', 'ANKR', 'SUSHI', 'ALGO', 'SC', 'WBTC', 'ONT', 'PPT', 'ONX', 'BTTC', 'RUB', 'PIVX', 'ASR', 'FIRO', 'AXSOLD', 'AST', 'MANA', 'DOTUP', 'ATA', 'MEETONE', 'QSP', 'ATD', 'NMR', 'MKR', 'DODO', 'LIT', 'ICP', 'ZEC', 'ATM', 'APPC', 'JEX', 'ICX', 'LOOM', 'ZEN', 'KP3R', 'DOGE', 'DUSK', 'ALPHA', 'BOLT', 'SXP', 'HBAR', 'RVN', 'MLN', 'AUD', 'LTOOLD', 'IDR', 'CTSI', 'KAVA', 'C98', 'PSG', 'HCC', 'VIDT', 'NOK', 'AVA', 'SYS', 'COCOS', 'STRAX', 'EOSUP', 'CZK', 'GAS', 'COVEROLD', 'AAVEDOWN', 'THETA', 'BCHUP', 'WAN', 'ORN', 'PERL', 'XLMDOWN', 'MASK', 'AAVE', 'GBP', 'PERP', '1INCHUP', 'SXPUP', 'YFIDOWN', 'BOND', 'YFI', 'PERLOLD', 'MOD', 'BICO', 'OST', 'XEC', 'YGG', 'PEOPLE', 'AXS', 'ZIL', 'VAI', 'XEM', 'CTXC', 'KEYFI', 'XTZUP', 'BIDR', 'BCHSV', 'AAVEUP', 'SUSHIDOWN', 'COMP', 'ETHBNT', 'OMOLD', 'OOKI', 'RUNE', 'FORTH', 'KMD', 'GHST', 'IDEX', 'DEXE', 'AVAX', 'UAH', 'KNC', 'PROS', 'PROM', 'BTCUP', 'CHAT', 'BGBP', 'LPT', 'HIVE', 'BIFI', 'PORTO', 'SNGLS', 'PYR', 'WAXP', 'DAI', 'YFIUP', 'DAR', 'FET', 'LRC', 'REPV1', 'ADXOLD', 'MTH', 'MTL', 'VET', 'ALPACA', 'USDT', 'USDS', 'OXT', 'USDP', 'DASH', 'NVT', 'SWRV', 'EDO', 'ILV', 'GHS', 'BTCST', 'HKD', 'JOE', 'LSK', 'KEEP', 'CAD', 'BEAM', 'CAN', 'DCR', 'CREAM', 'DATA', 'IMX', 'ENTRP', 'FILUP', 'UNIUP', 'LTC', 'USDC', 'WIN', 'LTCUP', 'INJ', 'TCT', 'PARA', 'LTO', 'VGX', 'TRIBE', 'NXS', 'EFI', 'DYDX', 'AGIX', 'INR', 'CBK', 'CBM', 'INS', 'POND', 'JPY', 'LINA', 'XLM', 'LINK', 'QTUM', 'FILDOWN', 'SUPER', 'UFT', 'POLS', 'KSM', 'LUN', 'FIL', 'POLY', 'STMX', 'RNDR', 'BAL', 'FIO', 'GALA', 'VIB', 'VIA', 'FIS', 'BAR', 'RAD', 'BAT', 'VRAB', 'AKRO', 'NZD', 'MOVR', 'XMR', '1INCHDOWN', 'COTI']
@@ -140,15 +136,17 @@ class n_book_saver:
         self.time_flag = self.tg.get_time_flag(self.time_pos)
         return self.time_flag
 
-    def upload_to_bucket(self, path_to_file, bucket_name="ndot_binance_stram"):
+    def upload_to_bucket(self, path_to_file, blob_name, bucket_name="ndot_binance_stram"):
         storage_client = storage.Client.from_service_account_json(
             'tribal-radar-284116-9bfd84d521e2.json')
+        # print(path_to_file)
         bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(path_to_file + ".pickle")
+        blob = bucket.blob(blob_name + ".pickle")
         blob.upload_from_filename(path_to_file + ".pickle")
         return blob.public_url
 
     def del_file(self, f_name):
+        # print("del fname", f_name)
         if os.path.isfile(f_name + ".pickle"):
             os.remove(f_name + ".pickle")
 
@@ -160,10 +158,13 @@ class n_book_saver:
         return psutil.virtual_memory().free / 1073741824  # return in GB
 
     def save_dict(self, sdict, f_name):
+        # print(len(sdict))
+        # print(len(str(sdict)))
         self.last_save_name = f_name
         pickle.dump(sdict, open(f_name + ".pickle", "wb"))
 
     def load_dict(self, name):
+        # print(name)
         return pickle.load(open(name + ".pickle", "rb"))
 
     def save_status(self):
@@ -253,20 +254,21 @@ class n_book_saver:
         # self.socket_thread.start()
 
     def n_start(self):
-        self.stream_dict["x"] = 12
-        print("cx len", len(tuple(self.stream_dict.keys())))
-        self.proc = multiprocessing.Process(target=self.start_asyc_websocket, args=(run_stream))
+
+        global global_stream_num
+        global global_result_dict
+
+        self.proc = multiprocessing.Process(target=self.start_asyc_websocket, args=(global_stream_num, global_result_dict))
         self.proc.start()
-        print("cx len", len(tuple(self.stream_dict.keys())))
 
     def n_stop(self):
         print("terminate")
         print(n_bs.time_flag)
         self.proc.terminate()
 
-    async def asyc_websocket(self):
-        global run_stream
-        print("asyc_websocket cx len", len(tuple(self.stream_dict.keys())))
+    async def asyc_websocket(self, global_stream_num, global_result_dict):
+
+        # print("asyc_websocket cx len", len(tuple(self.stream_dict.keys())))
 
         client = await AsyncClient.create()
         bm = BinanceSocketManager(client)
@@ -274,43 +276,58 @@ class n_book_saver:
         ts = bm.multiplex_socket(self.socket_list)
         async with ts as tscm:
             while True:
-                if run_stream:
+                # print("glob_num.value", global_stream_num.value)
+                if global_stream_num.value == 1:
                     res = await tscm.recv()
                     # self.all_pos += 1
                     res['datetime'] = datetime.datetime.now()
-                    self.stream_dict[self.stream_pos] = res
+                    global_result_dict[self.stream_pos] = res
                     self.stream_pos += 1
-                    print(run_stream)
-                        # print("in asyc_websocket cx len", len(tuple(self.stream_dict.keys())))
+                    # print("in asyc_websocket cx len", len(tuple(self.stream_dict.keys())))
                     # print(res)
+                    # glob_num.value = 0
                 else:
-                    print("False")
-                    await ts.__aexit__(None, None, None)
-                    await client.close_connection()
-                    del client
-                    del bm
-                    del ts
-                    return
 
-    def start_asyc_websocket(self, r):
-        print("start_asyc_websocket cx len", len(tuple(self.stream_dict.keys())))
+                    print("break")
+                    break
+        # print("close 1")
+        await ts.__aexit__(None, None, None)
+        # print("close 2")
+        await client.close_connection()
+        # print("close 3")
+        del client
+        # print("close 4")
+        del bm
+        # print("close 5")
+        del ts
+
+    def start_asyc_websocket(self, global_stream_num, global_result_dict):
+
+        # print("glob_num.value", global_stream_num.value)
+
         self.stream_pos = 0
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(self.asyc_websocket())
-        print("c1", self.stream_dict.keys())
-        loop.close()
-        print("c2", self.stream_dict.keys())
+        loop.run_until_complete(self.asyc_websocket(global_stream_num, global_result_dict))
+        time.sleep(10)
 
+        # loop.close() nem kell lezárni leáll magától
 
 
 if __name__ == '__main__':
 
-    n_bs = n_book_saver()
+    os.chdir('C:\\Coder\\ndotn_crypto_arbitrage')
 
+    global_stream_num = Value('d', 1.0)
+    # print("glob_num.value", global_stream_num.value)
+
+    manager = Manager()
+    global_result_dict = manager.dict()
+
+    n_bs = n_book_saver()
     dt_tag = datetime.datetime.now().strftime("%m%d_%H%M")
     sfile_name_prefix = "nDotBNC_" + dt_tag + "_" + str(n_bs.time_pos) + "_"
-    sfile_sufix = 0
+    sfile_sufix = n_bs.time_pos
 
     n_bs.save_status()
     last_time_flag = n_bs.time_flag
@@ -318,31 +335,52 @@ if __name__ == '__main__':
     status_chk = 1
     status_ping = status_chk * 60 / n_bs.chk_delay
 
+    # fname = "nDotBNC_0417_2018_0_0"
+    # x = n_bs.load_dict(fname)
+    # print("x len ", len(x))
+    # print(x[0])
+    # sys.exit(0)
+
     while True:
 
         t_flag = n_bs.update_time_flag()
-        print(t_flag)
-        if t_flag != 1:
-            print(n_bs.time_flag, datetime.datetime.now())
+        print(t_flag, datetime.datetime.now(), global_stream_num.value)
+        # if t_flag != 1:
+        #     print(t_flag, datetime.datetime.now())
 
         if t_flag == 1 and last_time_flag == 0:
             last_time_flag = t_flag
+            global_stream_num.value = 1
+            print("start stream")
+            global_result_dict = manager.dict()
             n_bs.n_start()
-            print("c-1", len(n_bs.stream_dict.keys()))
         if t_flag == 2:
-            run_stream = False
-            print("kint 1 cx len", len(tuple(n_bs.stream_dict.keys())))
-            time.sleep(15)
-            sys.exit()
-            n_bs.n_stop()
-            print("kint 2 cx len", len(tuple(n_bs.stream_dict.keys())))
-            fname = sfile_name_prefix + str(sfile_sufix)
-            sfile_sufix += 1
-            n_bs.save_dict(n_bs.stream_dict, fname)
+            global_stream_num.value = 0
+            # print("-" * 80)
+            # print("glob_num.value", global_stream_num.value)
+            # print("kint 1 cx len", len(tuple(n_bs.stream_dict.keys())))
+            time.sleep(10)
+            # sys.exit()
+            # n_bs.n_stop()
+            # print("kint 2 ----> len", len(tuple(global_result_dict.keys())))
 
-            n_bs.stream_dict = {}
-            n_bs.upload_to_bucket(fname)
-            print(n_bs.get_file_size(fname) / 1024 / 1024, "MB")
+            sdic = {}
+            for gd in global_result_dict:
+                sdic[gd] = global_result_dict[gd]
+
+            # print(global_result_dict[0])
+            fname = sfile_name_prefix + str(sfile_sufix)
+            blob = sfile_name_prefix + str(sfile_sufix)
+            sfile_sufix += 2
+            n_bs.save_dict(sdic, fname)
+
+            x = n_bs.load_dict(fname)
+            # print("x len ", len(x))
+            # print(x[0])
+
+            # n_bs.stream_dict = {}
+            n_bs.upload_to_bucket(fname, blob)
+            print(len(global_result_dict) / 1024 / 1024, "MB")
             n_bs.del_file(fname)
             gc.collect()
             n_bs.tg.shift_time_dict()
